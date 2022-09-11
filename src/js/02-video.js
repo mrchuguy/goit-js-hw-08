8d3c82092597938b246891ca341bd3297083f648
Sergey Chuguy <mr.chuguy@gmail.com>
2022-09-11 17:11:20 +0300

    finish second task

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,32 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+const save = (key, value) => {
+  try {
+    const serializedState = JSON.stringify(value);
+    localStorage.setItem(key, serializedState);
+  } catch (error) {
+    console.error('Set state error: ', error.message);
+  }
+};
+
+const load = key => {
+  try {
+    const serializedState = localStorage.getItem(key);
+    return serializedState === null ? undefined : JSON.parse(serializedState);
+  } catch (error) {
+    console.error('Get state error: ', error.message);
+  }
+};
+
+player.setCurrentTime(load('videoplayer-current-time'));
+
+player.on(
+  'timeupdate',
+  throttle(({ seconds }) => {
+    save('videoplayer-current-time', seconds);
+  }, 1000)
+);
